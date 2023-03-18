1a0538f9f224ed184c41faa690f80e1b7941b7d6
wiggle1000 <austintempletonthepawner@yahoo.com>
2023-03-17 21:13:44 -0500

    Updated capitalization of Includes

--- src/Chronos.cpp
+++ src/Chronos.cpp
@@ -4,7 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-#include "chronos.h"
+#include "Chronos.h"
 
 void Chronos::Init()
 {
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "pico/stdlib.h"
 #include "ResetFromBoot.h"
 
-#include "chronos.h"
+#include "Chronos.h"
 
 repeating_timer_t *audioRateTimer;
 
